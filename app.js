8b402bc44e1c2ba2e13b695293ca74ae5115cd39
andycr123 <81699558+andycr123@users.noreply.github.com>
2023-07-21 19:57:57 -0500

    changes

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
  var mysql = require('mysql');
 
  var app =  express();
-
+ const puerto = process.env.PUERTO;
  app.listen('3000', function(){
-        console.log("Servidor OK");
+    app.get( puerto, function(res, resq){
+        res.send('Ruta de inicio');
+    })
+        console.log("Servidor Ok en el puerto"+puerto);
  })
