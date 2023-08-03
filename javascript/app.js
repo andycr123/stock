fac9e2e7db0e2959210712c7ba77bcce1ec0fc61
andycr123 <81699558+andycr123@users.noreply.github.com>
2023-08-02 19:47:37 -0500

    changes

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,4 @@
- var express = require('express');
- var mysql = require('mysql');
 
-
- var conexion = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'tienda_onlie'
- });
-
- conexion.connect(function(error){
-    if(error){
-        
-    }
- })
-
- var app =  express();
- app.get( puerto, function(res, resq){
-     res.send('Ruta de inicio');
- })
-
-const puerto = process.env.PUERTO || 3000;
- app.listen('3000', function(){
-    console.log("Servidor Ok en el puerto"+puerto);
- })
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
